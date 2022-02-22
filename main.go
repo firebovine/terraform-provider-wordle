07b9b277f2f32dd33a14dc74b3eebaafd4127c51
firebovine <firebovine@gmail.com>
2022-02-21 20:53:55 -0500

    appease the linter

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
 	"terraform-provider-wordle/wordle"
@@ -9,8 +8,6 @@ import (
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() *schema.Provider {
-			return wordle.Provider()
-		},
+		ProviderFunc: wordle.Provider,
 	})
 }
