d72ddfc6769ea38fdb3a89193658ad974ccc663c
firebovine <firebovine@gmail.com>
2022-02-21 20:53:55 -0500

    appease the linter

--- wordle/data_source_wordle_test.go
+++ wordle/data_source_wordle_test.go
@@ -1,32 +1,32 @@
 package wordle
 
 import (
-  "fmt"
-  "testing"
+	"fmt"
+	"testing"
 
-  "github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccWordle(t *testing.T) {
-  resource.ParallelTest(t, resource.TestCase{
-	PreCheck: func() { /* no precheck needed testAccPreCheck(t) */ },
-	ProviderFactories: testAccProviderFactories,
-	Steps: []resource.TestStep{
-		{
-			Config: testAccDate("2022-01-01T00:00:00Z"),
-			Check: resource.ComposeTestCheckFunc(
-				resource.TestCheckResourceAttr(
-					"data.wordle_word.test", "word", "rebus"),
-				resource.TestCheckResourceAttrSet(
-					"data.wordle_word.test", "date"),
-			),
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { /* no precheck needed testAccPreCheck(t) */ },
+		ProviderFactories: testAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDate("2022-01-01T00:00:00Z"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.wordle_word.test", "word", "rebus"),
+					resource.TestCheckResourceAttrSet(
+						"data.wordle_word.test", "date"),
+				),
+			},
 		},
-	},
-  })
+	})
 }
 
 func testAccDate(date string) string {
-  return fmt.Sprintf(`
+	return fmt.Sprintf(`
 data "wordle_word" "test" {
   date = "%[1]v"
 }
